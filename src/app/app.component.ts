113fce683bfe61168758f4522995589878fe8da5
Mariia Zubkova <mariia_zubkova@epam.com>
2019-03-11 11:32:42 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'next-assay-pre-approved-study-details-lib-app';
+}
